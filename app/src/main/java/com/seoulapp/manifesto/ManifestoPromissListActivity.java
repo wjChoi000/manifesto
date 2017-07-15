e9031ed6d8bbfd342689b381521d6652903efb1e
wjChoi <wjChoi222@gmail.com>
2017-07-15 14:14:52 +0900

    update

---
+++ app/src/main/java/com/seoulapp/manifesto/ManifestoPromissListActivity.java
@@ -0,0 +1,13 @@
+package com.seoulapp.manifesto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManifestoPromissListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manifesto_promiss_list);
+    }
+}
