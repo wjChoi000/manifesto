c1636e71f54e243e53ddea9466bf761c6430c058
BoGyuPark <BoGyuPark>
2017-10-14 03:41:22 +0900

    image down update

--- app/src/main/java/com/seoulapp/manifesto/TabCitizen.java
+++ app/src/main/java/com/seoulapp/manifesto/TabCitizen.java
@@ -187,10 +187,10 @@ public class TabCitizen extends Fragment {
     private List<RollingModel> getData() {
         List<RollingModel> list = new ArrayList<>();
 
-        list.add(new RollingModel("1", R.drawable.listen_smoking_edit));
-        list.add(new RollingModel("2", R.drawable.listen_youth_edit));
-        list.add(new RollingModel("3", R.drawable.help_children_edit1));
-        list.add(new RollingModel("4", R.drawable.help_development_edit1));
+        list.add(new RollingModel("1", R.drawable.listen_smoking_edit2));
+        list.add(new RollingModel("2", R.drawable.listen_youth_edit2));
+        list.add(new RollingModel("3", R.drawable.help_children_edit2));
+        list.add(new RollingModel("4", R.drawable.help_development_edit2));
 
         return list;
     }
